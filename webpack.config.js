60a0896c4d4d43b8d4747cd297209c18e8e99c8c
BTConca <1512250@student.fit.hcmus.vn>
2019-11-16 10:58:17 +0700

    Fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.export = {
+module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index-bundle.js"
   },
   module: {
-    rule: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: /node-modules/,
+        exclude: /node_modules/,
         use: ["babel-loader"]
       },
       {
         test: /\.css$/,
-        use: ["style-loader"]
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
-  plugin: [
+  plugins: [
     new HtmlWebpackPlugin({
-      tempalte: "./src/index.html"
+      template: "./src/index.html"
     })
   ]
 };
